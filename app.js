9494f3d38101bb754fafe996c409f9332fa842bc
Jonathon Chambers <FFCoder@users.noreply.github.com>
2019-12-29 23:00:01 -0500

    Version 1.0 complete

---
+++ app.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const DNS_ADDRESS = `https://api.digitalocean.com/v2/domains/jonathonchambers.com/records/${process.env.RECORD_ID}`
+const API_KEY = process.env.DIGITALOCEAN_OAUTH_KEY;
+const RECORD_NAME = process.env.RECORD_NAME;
+// Just a variable to hold the number of mil
+const ONE_HOUR = 3600000;
+
+const updateDNS = async() => {
+    var externalIP = await axios.get('https://api.ipify.org').then(res => res.data);
+    console.log("External IP: ", externalIP);
+    var headers = {
+        Authorization: `Bearer ${API_KEY}` 
+    }
+
+    var data = {
+        name: RECORD_NAME,
+        data: externalIP
+
+    }
+    await axios.put(DNS_ADDRESS, data, {headers: headers}).then(res => {
+        if (res.data.domain_record) {
+            console.log("Record Updated")
+        }
+        else  {
+            console.error("An Unknown Error Occured: ", res.data)
+        }
+    }).catch(err => {
+        console.error(err)
+    })
+}
+console.log('Starting');
+updateDNS();
